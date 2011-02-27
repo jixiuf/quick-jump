825e4488f720b6e40622d31f08a440340d36b7de
jixiuf <jixiuf@gmail.com>
2011-02-27 11:50:57 +0800

    edit Commentary and Install

--- quick-jump.el
+++ quick-jump.el
@@ -1,4 +1,4 @@
-;;; quick-jump.el ---Remember current position,and jump back cyclely.
+;;; quick-jump.el ---Remember current position,and jump back .
 
 ;; Filename: quick-jump.el
 ;; Description:Remember current position,and jump back cyclely.
@@ -32,7 +32,77 @@
 ;; Floor, Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
+;;   quick-jump.el works like `register' in Emacs. but it is
+;;   different to it ,quick-jump.el push current position to a
+;;   ring named `qj-marker-ring' then you can go back(or forward)
+;;   to the position ,and you can clear all the position in `qj-marker-ring'
+;;   to manager the ring by `quick-jump-clear-all-marker'.
 ;;
+;;   Suppose you have run `quick-jump-push-marker' on line 1,5,10
+;;   then three positions is pushed into `qj-marker-ring'. now the ring is
+;;   [10,5,1],you  are on line 10 now ,then you run  `quick-jump-go-back'
+;;   you will be back to line 5, the ring still is [10,5,1]. but
+;;   `qj-current-marker' is not the last position you have pushed on line
+;;   10, it's line 5 now.
+;;   go back and go forward are related to `qj-current-marker'.
+;;
+;;   when you `quick-jump-go-back' or `quick-jump-go-forward' 
+;;   it will decide whether push current position to `qj-marker-ring'
+;;   automaticly or not . when the count of lines between current line
+;;   and the line of `qj-current-marker' are larger than `qj-line-count',
+;;   then current position will be automaticly put into `qj-marker-ring'
+;;
+;;   So suppose the ring is [15,10,5,1] and `qj-current-marker' is 10 ,if you
+;;   are on line 100 now ,and you run `quick-jump-go-back' now you position
+;;   is go back to line 5,the ring become [100,15,10.5.1],because
+;;   100-10>`qj-line-count' it is larger than `qj-line-count' default value
+;;   10.but if you are on line 16 and run `quick-jump-go-back', the ring still
+;;   is [15,10,5,1] ,it isn't [16,15,10,5,1],because 16-10<`qj-line-count'.
+;;
+;;
+;;   此段代码用于快速跳转,主要用于代码浏览或代码编写时在多个大跨度的位置间进行切换时使用
+;;   以我的键绑定为例
+;;      (global-set-key (kbd "C-,") 'quick-jump-go-back)
+;;      (global-set-key (kbd "C-.") 'quick-jump-push-marker)
+;;      (global-set-key (kbd "C->") 'quick-jump-clear-all-marker))
+;;      (global-set-key (kbd "C-<") 'quick-jump-go-forward)
+;;   
+;;   最简单的使用方法
+;;   `C-.' 会记录当前光标的位置
+;;   `C-,' 跳回到`C-.' 所记录的位置
+;;   当然你可以在文件不同的位置多处按下`C-.' ,而`C-,'则会按照先后顺序遍历这些位置
+;;   
+;;   但上面的使用方法有个弊端,必须先经`C-.' 保存位置后,才可以`C-,' 进行跳转,有时会忘掉`C-.',以
+;;    致无法跳转到这个位置.
+;;   为了解决这个问题 ,`C-,'不但可以进行跳转,也会根据当前的位置在特定的情况下自动将当前位置加入到
+;;   循环队列中
+
+;;; Install:
+;;
+;; Just put quick-jump.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'quick-jump)
+;; (quick-jump-default-keybinding)
+;;
+;; or 
+;;   
+;; (autoload 'quick-jump-push-marker "quick-jump"
+;;   " push current marker in ring. you can jump back" t)
+;; (autoload 'quick-jump-go-back "quick-jump"
+;;   "Go back in `qj-marker-ring'")
+;; (autoload 'quick-jump-go-forward "quick-jump"
+;;   "Go forward in `qj-marker-ring'")
+;; (autoload 'quick-jump-clear-all-marker "quick-jump"
+;;   "clear all marker in `qj-marker-ring'.")
+;; (autoload 'quick-jump-default-keybinding "quick-jump"
+;;   "default keybindings for quick-jump" nil)
+;; (quick-jump-default-keybinding)
+   
 
 ;;; Commands:
 ;;
@@ -40,9 +110,9 @@
 ;;
 ;;  `quick-jump-push-marker'
 ;;    push current marker in ring. you can jump back
-;;  `quick-jump-history-go-back'
+;;  `quick-jump-go-back'
 ;;    Go back in `qj-marker-ring'.
-;;  `quick-jump-history-go-forward'
+;;  `quick-jump-go-forward'
 ;;    Go forward in `qj-marker-ring'.
 ;;  `quick-jump-clear-all-marker'
 ;;    clear all marker in `qj-marker-ring'.
@@ -51,32 +121,24 @@
 ;;
 ;; Below are customizable option list:
 ;;
+;;  `qj-line-count'
+;;    about the meaning of me see Commentary.
+;;    default = 10
 
-;;此段代码用于快速跳转,主要用于代码浏览或代码编写时在多个大跨度的位置间进行切换时使用
-;;以我的键绑定为例
-;;
-;; (global-set-key (kbd "C-.") 'quick-jump-push-marker)
-;;
-;; (global-set-key (kbd "C-,") 'quick-jump-history-go-back)
-;; (global-set-key (kbd "C-<") 'quick-jump-history-go-forward)
-;;
-;;最简单的使用方法
-;;`C-.' 会记录当前光标的位置
-;;`C-,' 跳回到`C-.' 所记录的位置
-;;当然你可以在文件不同的位置多处按下`C-.' ,而`C-,'则会按照先后顺序遍历这些位置
-;;
-;;
-;;但上面的使用方法有个弊端,必须先经`C-.' 保存位置后,才可以`C-,' 进行跳转,有时会忘掉`C-.',以
-;; 致无法跳转到这个位置.
-;;为了解觉这个问题 ,`C-,'不但可以进行跳转,也会根据当前的位置在特定的情况下自动将当前位置加入到
-;;循环队列中.
-;;(print qj-marker-ring)
 (require 'ring)
+
+(defgroup quick-jump nil
+  "Another Etags anything.el interface."
+  :group 'convenience)
+
+(defcustom  qj-line-count 10
+  "about the meaning of me see Commentary."
+  :type 'number
+  :group 'quick-jump)
 
 (defvar qj-current-marker nil)
 (defvar qj-marker-ring (make-ring 5))
 (defvar qj-previous-action-flag nil)
-(defvar qj-line-count 10)
 
 ;;; util func
 (defun qj-is-marker-available(marker)
@@ -93,7 +155,7 @@
   `(let ((it ,test-form))
      (if it ,then-form ,@else-forms)))
 ;;; funcs
-(defun qj-history-init()
+(defun qj-init()
   "remove #<marker in no buffer> from `qj-marker-ring'."
       (let ((tmp-marker-ring))
         (while (not (ring-empty-p qj-marker-ring))
@@ -115,7 +177,7 @@
 
 
 
-(defun qj-history-action-go(marker)
+(defun qj-action-go(marker)
   "Go to location."
   (let ((buf (marker-buffer marker))
         (pos (marker-position marker)))
@@ -127,17 +189,17 @@
 
 (defun quick-jump-push-marker()
   "push current marker in ring. you can jump back
-by `quick-jump-history-go-back'"
+by `quick-jump-go-back'"
   (interactive)
   (when (not (ring-member qj-marker-ring (point-marker)))
     (ring-insert qj-marker-ring (point-marker)))
   (setq qj-current-marker (point-marker))
   (message "a marker is pushed."))
 
-(defun quick-jump-history-go-back()
+(defun quick-jump-go-back()
   "Go back in `qj-marker-ring'."
   (interactive)
-  (qj-history-init)
+  (qj-init)
   (when (and
          (qj-is-marker-available qj-current-marker)
          (ring-member qj-marker-ring qj-current-marker))
@@ -147,15 +209,15 @@ by `quick-jump-history-go-back'"
       (setq qj-current-marker (ring-next qj-marker-ring qj-current-marker)))
     
     (message (concat  "jump to " (prin1-to-string qj-current-marker)))
-    (qj-history-action-go qj-current-marker)
+    (qj-action-go qj-current-marker)
     
     (setq qj-current-marker (ring-next qj-marker-ring qj-current-marker))
     (setq qj-previous-action-flag "back")))
 
-(defun quick-jump-history-go-forward()
+(defun quick-jump-go-forward()
   "Go forward in `qj-marker-ring'."
   (interactive)
-  (qj-history-init)
+  (qj-init)
   (when (and
          (qj-is-marker-available qj-current-marker)
          (ring-member qj-marker-ring qj-current-marker))
@@ -165,7 +227,7 @@ by `quick-jump-history-go-back'"
       (setq qj-current-marker (ring-previous qj-marker-ring qj-current-marker)))
     
     (message (concat  "jump to " (prin1-to-string qj-current-marker)))
-    (qj-history-action-go qj-current-marker)
+    (qj-action-go qj-current-marker)
     
     (setq qj-current-marker (ring-previous qj-marker-ring qj-current-marker))
     (setq qj-previous-action-flag "forward")))
@@ -181,10 +243,11 @@ by `quick-jump-history-go-back'"
     (ring-remove qj-marker-ring)))
 
 
-(global-set-key (kbd "C-,") 'quick-jump-history-go-back)
-(global-set-key (kbd "C-.") 'quick-jump-push-marker)
-(global-set-key (kbd "C-<") 'quick-jump-history-go-forward)
-(global-set-key (kbd "C->") 'quick-jump-clear-all-marker)
+(defun quick-jump-default-keybinding()
+  (global-set-key (kbd "C-,") 'quick-jump-go-back)
+  (global-set-key (kbd "C-.") 'quick-jump-push-marker)
+  (global-set-key (kbd "C-<") 'quick-jump-go-forward)
+  (global-set-key (kbd "C->") 'quick-jump-clear-all-marker))
 
 (provide 'quick-jump)
 
